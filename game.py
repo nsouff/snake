04346d4295ce8643c7e4a4e9202c3c2c74a5a0a4
Nathan Souffan <souffan.nathan@gmail.com>
2020-05-23 16:09:28 +0200

    Randomize the add of food

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from snake import *
 from linked_list import *
+import random
 
 class Game():
     def __init__(self, width=80, height=50):
@@ -11,8 +12,23 @@ class Game():
         self.grid = [[0 for _ in range(height)] for _ in range(width)]
         self.snake = Snake(self.grid)
     def add_food(self):
-        self.foods.append((1, 5))
-        self.grid[1][5] = 2
+        x = random.randint(0, self.width-1)
+        y = random.randint(0, self.height-1)
+        if self.grid[x][y] == 0:
+            self.grid[x][y] = 2
+            self.foods.append((x, y))
+        else:
+            while self.grid[x][y] != 0:
+                if y < height - 1:
+                    y += 1
+                elif x < width - 1:
+                    y = 0
+                    x += 1
+                else:
+                    x = 0
+                    y = 0
+            self.grid[x][y] = 2
+            self.foods.append((x, y))
 def update_render(game, window):
     window.fill((0,0,0))
     it = game.snake.pos.first
