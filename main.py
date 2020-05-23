17eb8411b8a9871b011b33188ef284aaec95f714
Nathan Souffan <souffan.nathan@gmail.com>
2020-05-24 00:42:11 +0200

    Add simple menu from pygame_menu module

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
+import pygame_menu
 from snake import *
 from game import *
 from linked_list import *
 from pygame.locals import *
-
+import sys
 def update_render(game, window):
     window.fill((0,0,0))
     it = game.snake.pos.first
@@ -13,22 +14,19 @@ def update_render(game, window):
     for food in game.foods:
         pygame.draw.circle(window, (255, 0, 0), (food[0]*10+5, food[1]*10+5), 5)
     pygame.display.flip()
-def main():
+
+def gameloop(window):
     game = Game()
-    pygame.init()
-    pygame.display.set_caption("Snake")
-    window = pygame.display.set_mode((game.width*10, game.height*10))
     clock = pygame.time.Clock()
-    update_render(game, window)
-    running = 1
-    next_food = pygame.time.get_ticks() + 5000
     eat_sound = pygame.mixer.Sound("resources/eat_apple.wav")
+    running = True
+    next_food = pygame.time.get_ticks() + 5000
     while running:
         dir_change = False
         for event in pygame.event.get():
-
             if event.type == QUIT:
-                running = 0
+                pygame.quit()
+                sys.exit()
             elif event.type == KEYDOWN and not dir_change:
                 if event.key == K_UP:
                     dir_change = game.snake.turn(Direction.N)
@@ -40,7 +38,7 @@ def main():
                     dir_change = game.snake.turn(Direction.W)
         i = game.snake.update()
         if i == 0:
-            running = 0
+            running = False
         else :
             if i == 2:
                 game.foods.remove(game.snake.pos.first.item)
@@ -51,5 +49,16 @@ def main():
                 next_food = pygame.time.get_ticks() + 5000
             update_render(game, window)
             clock.tick(game.snake.speed)
+def menu(window):
+    menu = pygame_menu.Menu(400, 400, 'Menu', onclose=pygame_menu.events.EXIT ,theme=pygame_menu.themes.THEME_DARK)
+    menu.add_button('Play', gameloop, window)
+    menu.add_button('Quit', pygame_menu.events.EXIT)
+    menu.mainloop(window)
+def main():
+    pygame.init()
+    pygame.display.set_caption("Snake")
+    window = pygame.display.set_mode((400, 400))
+    menu(window)
+
 if __name__ == '__main__':
     main()
