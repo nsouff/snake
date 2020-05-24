ad3a1566479a3c3e107734d187959a5a54c5d9dd
Nathan Souffan <souffan.nathan@gmail.com>
2020-05-24 18:50:35 +0200

    Add a score manager

--- game.py
+++ game.py
@@ -29,3 +29,31 @@ class Game():
                     y = 0
             self.grid[x][y] = 2
             self.foods.append((x, y))
+
+class Score():
+    def __init__(self, scores=None, max_len=10, file='.score'):
+        self.file = file
+        self.max_len = max_len
+        if scores != None:
+            self.scores = scores
+        else:
+            self.scores = []
+    def add_score(self, name, score):
+        act_len = len(self.scores)
+        if self.max_len > act_len or score >= self.scores[self.max_len-1][1]:
+            self.__add_score_aux(name, score)
+            if act_len == self.max_len:
+                self.scores.pop()
+    def __add_score_aux(self, name, score, start=None, end=None):
+        if start == None:
+            start = 0
+        if end == None:
+            end = len(self.scores)
+        if end == start:
+            self.scores = self.scores[:start] + [(name, score)] + self.scores[start:]
+            return
+        middle = (end + start) // 2
+        if score < self.scores[middle][1]:
+            return self.__add_score_aux(name, score, start=middle+1, end=end)
+        else:
+            return self.__add_score_aux(name, score, start=start, end=middle)
--- game_test.py
+++ game_test.py
@@ -10,6 +10,23 @@ class GameTest(unittest.TestCase):
         for i in range(x):
             for j in range(y):
                 self.assertNotEqual(game.grid[i][j], 0)
-
+class ScoreTest(unittest.TestCase):
+    def test_add_score(self):
+        score = Score()
+        score.add_score('Nathan', 10)
+        score.add_score('Nathan', 12)
+        score.add_score('Nat', 5)
+        score.add_score('N', 20)
+        score.add_score('Nathan', 0)
+        score.add_score('N', 1)
+        score.add_score('a', 19)
+        score.add_score('b', 15)
+        score.add_score('c', 0)
+        score.add_score('c', 20)
+        score.add_score('d', 15)
+        score.add_score('d', 15)
+        self.assertEqual(len(score.scores), 10)
+        scores = [('c', 20), ('N', 20), ('a', 19), ('d', 15), ('d', 15), ('b', 15), ('Nathan', 12), ('Nathan', 10), ('Nat', 5), ('N', 1)]
+        self.assertListEqual(score.scores, scores)
 if __name__ == '__main__':
     unittest.main()
